fab4d104fa5bbef5e20749ae240257146d138e29
Avinash Daby <djavinash@yahoo.co.uk>
2024-07-23 20:36:25 +0400

    Completed Ex2 Stupid Coaching  modified questions_controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,13 @@
 class QuestionsController < ApplicationController
 
-  def ask
-  end
+  def ask; end
 
   def answer
     @question = params[:question]
 
     if @question.include?("?")
       @answer = 'Silly question, get dressed and go to work!'
-    elsif @question == 'I am going to work'
+    elsif @question.capitalize == 'I am going to work'
       @answer = 'Great!'
     else
       @answer = "I don't care, get dressed and go to work!"
